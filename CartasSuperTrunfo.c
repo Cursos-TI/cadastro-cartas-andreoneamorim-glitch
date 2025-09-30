e21e8ff1622e7afd32670d6d8ed1bc541ff59412
andreoneamorim-glitch <andreone.amorim@gmail.com>
2025-09-29 23:52:50 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,22 +20,16 @@ int main() {
     printf("Cadastro da carta cidade 1:\n");
     printf("Digite o estado (A-H): ");
     scanf(" %c", &estado_1);
-
     printf("Digite o código da carta 1: ");
     scanf("%s", codigo_1);
-
     printf("Digite o nome da cidade: ");
     scanf(" %[^\n]", cidade_1); // lê-se string com espaços
-
     printf("Digite a população: ");
     scanf("%d", &populacao_1);
-
     printf("Digite a área em km²: ");
     scanf("%f", &area_1);
-
     printf("Digite o PIB (em bilhões de reais): ");
     scanf("%f", &pib_1);
-
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos_1);
 
@@ -43,22 +37,16 @@ int main() {
     printf("\nCadastro da Carta 2:\n");
     printf("Digite o estado (A-H): ");
     scanf(" %c", &estado_2);
-
     printf("Digite o código da carta 2: ");
     scanf("%s", codigo_2);
-
     printf("Digite o nome da cidade 2: ");
     scanf(" %[^\n]", cidade_2);
-
     printf("Digite a população: ");
     scanf("%d", &populacao_2);
-
     printf("Digite a área em km² 2: ");
     scanf("%f", &area_2);
-
     printf("Digite o PIB (em bilhões de reais): ");
     scanf("%f", &pib_2);
-
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos_2);
   
